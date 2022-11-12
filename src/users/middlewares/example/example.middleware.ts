ef2b2569021c197545363f993ef845b7b81c919b
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-12 14:33:19 -0300

    feat: :sparkles: cria um middleware para rota users

--- src/users/middlewares/example/example.middleware.ts
+++ src/users/middlewares/example/example.middleware.ts
@@ -3,6 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class ExampleMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    console.log('Example Middleware');
     next();
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
+import { ExampleMiddleware } from './middlewares/example/example.middleware';
 import { UsersService } from './services/users/users.service';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ExampleMiddleware).forRoutes('users');
+  }
+}
